7c6c0c738f5e6cb96011e68b82175784a74077e6
Shubham <shubhamchatterji47.com>
2026-05-08 11:46:56 +0530

    fix: add environment.prod.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 // src/environments/environment.ts
-export const environment = {
-  production: false,
-  apiUrl: 'https://cinestream-api-production.up.railway.app'
-};
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://cinestream-api-production.up.railway.app'
+// };
 
 // src/environments/environment.prod.ts
 // export const environment = {
