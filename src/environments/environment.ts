83037521a16dcbf782d880ed18c31a282e75d351
Shubham <shubhamchatterji47.com>
2026-05-09 18:23:16 +0530

    fix: clean environment.ts remove comments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,1 @@
-// src/environments/environment.ts
-// export const environment = {
-//   production: false,
-//   apiUrl: 'https://cinestream-api-production.up.railway.app'
-// };
-
-// src/environments/environment.prod.ts
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://your-production-api.com'
-// };
-export const environment = {
-  production: false,
-  apiUrl: 'https://cinestream-api-production.up.railway.app'
-};
+export const environment = { production: false, apiUrl: 'https://cinestream-api-production.up.railway.app' }; 
