e68e6874236df3dd0263497a2678b75279d1e7f9
Shubham <shubhamchatterji47.com>
2026-05-10 13:16:49 +0530

    fix: use Render API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://cinestream-api-production.up.railway.app'
+// };
 export const environment = {
   production: true,
-  apiUrl: 'https://cinestream-api-production.up.railway.app'
+  apiUrl: 'https://cinestream-api.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://cinestream-api-production.up.railway.app'
+// };
 export const environment = {
   production: false,
-  apiUrl: 'https://cinestream-api-production.up.railway.app'
+  apiUrl: 'https://cinestream-api.onrender.com'
 };
